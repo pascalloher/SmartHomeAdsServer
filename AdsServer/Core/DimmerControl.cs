32c2ea16ee717a36715476caf7c4a1e2e7fe7b32
pascalloher <pascal.loher@gmail.com>
2021-12-18 15:08:58 +0100

    Update auf .net 6

---
+++ AdsServer/Core/DimmerControl.cs
@@ -0,0 +1,23 @@
+﻿using TwinCAT.Ads;
+
+namespace AdsServer1.Core
+{
+    public class DimmerControl : IDimmerControl
+    {
+        public async Task SetAsync(int dimmer, uint value, CancellationToken token)
+        {
+            using AdsClient client = new();
+            client.Connect(AmsNetId.Local, 851);
+            await client.WriteValueAsync($"Main.Dimmer[{dimmer}]", value, token);
+        }
+
+        public async Task<uint> GetAsync(int dimmer, CancellationToken token)
+        {
+            using AdsClient client = new();
+            client.Connect(AmsNetId.Local, 851);
+            ResultValue<uint> readResult = await client.ReadValueAsync<uint>($"Main.Dimmer[{dimmer}]", token);
+            readResult.ThrowOnError();
+            return readResult.Value;
+        }
+    }
+}
---
+++ AdsServer/Core/IDimmerControl.cs
@@ -0,0 +1,8 @@
+﻿namespace AdsServer1.Core
+{
+    public interface IDimmerControl
+    {
+        Task SetAsync(int dimmer, uint value, CancellationToken token);
+        Task<uint> GetAsync(int dimmer, CancellationToken token);
+    }
+}
